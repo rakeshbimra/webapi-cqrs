c0857248a78272987eacbda42d679f19ab386e73
rakeshbimra <rakeshbimra@gmail.com>
2020-03-08 00:17:57 +0530

    added unit tests for UnitOfWork

---
+++ MyApp.Context.Domain.Tests/UnitOfWorkTest.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Moq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyApp.Context.Domain.Tests
+{
+    public class UnitOfWorkTest
+    {
+        [Test]
+        public void UnitOfWork_Call_SaveChanges()
+        {
+            var mockContext = new Mock<DbContext>();
+            var unitOfWork = new UnitOfWork<DbContext>(mockContext.Object);
+            unitOfWork.SaveChanges();
+            mockContext.Verify(x => x.SaveChanges());
+        }
+    }
+}
